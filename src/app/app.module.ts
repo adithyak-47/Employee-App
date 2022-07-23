3623f43df04cadaba88bbddfa5e6051a07edd9aa
Adithyakrishnan <adithyakrishnannanda@gmail.com>
2022-07-23 22:10:00 +0530

    New App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { ViewemployeeComponent } from './viewemployee/viewemployee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddemployeeComponent,
+    ViewemployeeComponent
   ],
   imports: [
     BrowserModule,
